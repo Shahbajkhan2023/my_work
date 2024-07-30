c61ebee06b4c440038fae6843002a58bc41819f0
shahbajkhan2023 <rk2403476@gmail.com>
2024-07-30 17:42:58 +0530

    this new branch model_one_to_relation

--- model_prac/models.py
+++ model_prac/models.py
@@ -39,3 +39,18 @@ class Product1(models.Model):
 
 
 
+
+class User(models.Model):
+    name = models.CharField(max_length=50)
+   
+    def __str__(self):
+        return str(self.name)
+    
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.PROTECT, primary_key=True)
+    language = models.CharField(max_length=50)
+    email = models.EmailField(max_length=70, blank=True, unique=True)
+
+    def __str__(self):
+        return str(self.email)
